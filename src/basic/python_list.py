e575e0c86a52b01391faf8ae9179b087ba1c38e7
jingrongx <jingrongx.xu@intel.com>
2019-04-30 16:51:23 +0800

    增加更多list用法

--- src/basic/python_list.py
+++ src/basic/python_list.py
@@ -5,16 +5,45 @@ import time
 import datetime
 import calendar
 
-def output(info):
-    print(info)
+def loginfo(logString):
     # print now time
-    print(datetime.datetime.now().strftime("%Y-%m-%d %X.%f"))
+    print('%s: %s' % (datetime.datetime.now().strftime("%Y-%m-%d %X.%f"), logString))
 
 def main():
     udisk = ['SanDisk', 'Kingston']
-    output(udisk)
-    output(udisk[0])
-    output(udisk[1])
+    loginfo(udisk)
+    loginfo(udisk[0])
+    loginfo(udisk[1])
+    loginfo(udisk[-1])
+    loginfo(udisk[-2])
+    # add
+    udisk.append('Lenovo')
+    loginfo(udisk)
+
+    # insert
+    udisk.insert(1, 'HP')
+    loginfo(udisk)
+
+    # del
+    udisk.pop()
+    loginfo(udisk)
+    udisk.pop(1)
+    loginfo(udisk)
+
+    # replace
+    udisk[1] = 'Lenovo'
+    loginfo(udisk)
+
+
+    testListMix = ['desktop', 123, True]
+    loginfo(testListMix)
+
+    testListMul = ['python', 'java', ['jsp', 'php'], 'scheme']
+    loginfo(len(testListMul))
+
+    testListEmpty = []
+    loginfo(testListEmpty)
+    loginfo(len(testListEmpty))
 
 if __name__ == '__main__':
     main()
