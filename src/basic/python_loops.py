3baae0a93ad69a0ff092b0123a1c566a4dd18b79
jingrongx <jingrongx.xu@intel.com>
2019-07-09 12:16:33 +0800

    add python loops fun

--- src/basic/python_loops.py
+++ src/basic/python_loops.py
@@ -32,20 +32,29 @@ class r_log():
     logger.addHandler(fh)
 
 
-def conditions(*args):
-    # if <条件判断1>:
-    #     <执行1>
-    # elif <条件判断2>:
-    #     <执行2>
-    # elif <条件判断3>:
-    #     <执行3>
-    # else:
-    #     <执行4>
-    for i in args:
+def loops(a):
+    for i in a:
         if i<3:
             r_log.logger.info('a<3')
         else:
             r_log.logger.info('a>=3')
+
+    sum = 0
+    for x in range(101):
+        sum = sum + x
+    r_log.logger.info(sum)
+
+    sum = 0
+    n = 99
+    while n > 0:
+        sum = sum + n
+        n = n - 2
+    r_log.logger.info(sum)
+
+    name = ["a", "b", "c"]
+    for x in name:
+        message = "Hello: " + x
+        r_log.logger.info(message)
 
 
 def main():
@@ -55,15 +64,9 @@ def main():
     r_log.logger.warning('warn message')
     r_log.logger.error('error message')
     r_log.logger.critical('critical message')
-    a=1
-    b=2
-    c=3
-    d=4
-    e=5
-    f=6
-    g=7
-    h=int(input('h: '))
-    conditions(a, b, c, d, e, f, g, h)
+    l = [1, 2, 3]
+    loops(l)
+
 
 if __name__ == '__main__':
     main()
