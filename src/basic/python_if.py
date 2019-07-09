e4c29df42ed868b76f83bb964fa9f97c8d9cb5a6
jingrongx <jingrongx.xu@intel.com>
2019-07-09 11:52:53 +0800

    add conditions fun

--- src/basic/python_if.py
+++ src/basic/python_if.py
@@ -32,6 +32,22 @@ class r_log():
     logger.addHandler(fh)
 
 
+def conditions(*args):
+    # if <条件判断1>:
+    #     <执行1>
+    # elif <条件判断2>:
+    #     <执行2>
+    # elif <条件判断3>:
+    #     <执行3>
+    # else:
+    #     <执行4>
+    for i in args:
+        if i<3:
+            r_log.logger.info('a<=3')
+        else:
+            r_log.logger.info('a>3')
+
+
 def main():
     # 'application' code
     r_log.logger.debug('debug message')
@@ -39,6 +55,15 @@ def main():
     r_log.logger.warning('warn message')
     r_log.logger.error('error message')
     r_log.logger.critical('critical message')
+    a=1
+    b=2
+    c=3
+    d=4
+    e=5
+    f=6
+    g=7
+    h=int(input('h: '))
+    conditions(a, b, c, d, e, f, g, h)
 
 if __name__ == '__main__':
     main()
